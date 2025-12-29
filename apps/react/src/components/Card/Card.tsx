5d056e73677e3f48897f279e2b77801c147350bc
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-12-29 23:47:26 +0200

    React and Express Monorepo with pnpm

---
+++ apps/react/src/components/Card/Card.tsx
@@ -0,0 +1,8 @@
+type CardProps = {
+    title: string,
+    paragraph: string
+}
+
+export const Card = ({ title, paragraph }: CardProps) => <aside>
+    <span>{ title }</span>-<span>{ paragraph }</span>
+</aside>

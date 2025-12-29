c0709959c9d7346c766ab877b98ffe29737f314d
serhij.zolotukhin <serhij.zolotukhin@gmail.com>
2025-12-30 00:32:47 +0200

    React and Express Monorepo with pnpm

--- apps/express/src/index.ts
+++ apps/express/src/index.ts
@@ -1,40 +1,32 @@
 import express from "express";
-import path from "path";
-
-import fs from "fs";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Middleware
+interface Card {
+  id: number;
+  title: string;
+  paragraph: string;
+}
+
+let cards: Card[] = [];
+
+// Use the express.json() middleware
+// This must be declared before any route handlers that need to parse JSON bodies
 app.use(express.json());
 
-// Serve static files from the React frontend
-app.use(express.static(path.join(__dirname, "../../react/dist")));
-
 // Routes
-app.get("/api", (req, res) => {
-  res.send("Hello, World!" + process.env.NODE_ENV);
-});
+app.get("/api/tasks", (req, res) => {
+  for (let i = 0; i < 10; i++) {
+    const card: Card = {
+      id: cards.length + 1,
+      title: `Title ${i}`,
+      paragraph: `Paragraph ${i}`,
+    };
+    cards.push(card);
+  }
 
-app.get("/api/health", (req, res) => {
-  res.send("OK");
-});
-
-// Fallback for React Router (unmatched routes)
-app.use((req, res) => {
-  const filePath = path.join(__dirname, "../../react/dist", "index.html");
-
-  // Check if the file exists
-  fs.access(filePath, fs.constants.F_OK, (err) => {
-    if (err) {
-      // If file not found, send a fallback page or 404 response
-      res.status(404).send("Page not found");
-    } else {
-      // Serve the index.html if the file exists
-      res.sendFile(filePath);
-    }
-  });
+  res.status(201).json(cards);
 });
 
 // Start server
